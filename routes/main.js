743ef6f1eabbd6aeb22a161650bec1b0cf83e9ec
Lucatonello <lucatonellow@gmail.com>
2024-09-22 13:33:13 -0300

    added db to store times

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db/pool');
 
 router.post('/guess/:character', async (req, res) => {
     let character = req.params.character; 
@@ -9,11 +10,11 @@ router.post('/guess/:character', async (req, res) => {
     let correctY = null;
 
     if (character === 'waldo') {
-        correctX = 1241;
-        correctY = 707;   
-    } else if (character === 'wizzard') {
-        correctX = 1858;
-        correctY = 111;
+        correctX = 773;
+        correctY = 529;   
+    } else if (character === 'wizard') {
+        correctX = 115;
+        correctY = 864;
     } else {
         return res.status(400).json({ message: "Invalid character" });
     }
@@ -22,11 +23,23 @@ router.post('/guess/:character', async (req, res) => {
         guessX >= correctX - 40 && guessX <= correctX + 40 &&
         guessY >= correctY - 40 && guessY <= correctY + 40
     ) {
-        res.json({ succes: true, message: 'Correct!'}); 
+        res.json({ success: true, message: 'Correct!' }); 
     } else {
-        res.json({ succes: false, message: 'Try again!'});
+        res.json({ success: false, message: 'Try again!'});
     }
 
+});
+
+router.post('/addtime', async (req, res) => {
+    try {
+        const playername = req.body.playerName;
+        const time = req.body.time;
+    
+        await pool.query('INSERT INTO times (playername, time) VALUES ($1, $2)', [playername, time]);
+        res.json({ message: 'successful database operation'});    
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 module.exports = router;
